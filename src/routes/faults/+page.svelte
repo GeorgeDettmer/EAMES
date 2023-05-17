4466a430df5c8a2e9a1f4e7050614978709a73f7
George <george.dettmer@gmail.com>
2023-05-17 23:23:10 +0100

    Test

--- src/routes/faults/+page.svelte
+++ src/routes/faults/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { page } from '$app/stores';
 
 	export let data: PageData;
 
@@ -8,8 +9,8 @@
 	const faultsQueryStore = queryStore({
 		client: getContextClient(),
 		query: gql`
-			query Faults {
-				faults(batch: "May23") {
+			query Faults($batch: String) {
+				faults(batch: $batch) {
 					ID
 					JobNumber
 					SerialNumber
@@ -22,8 +23,20 @@
 					Reworked
 				}
 			}
-		`
+		`,
+		variables: {
+			batch: $page.url.searchParams.get('batch')
+		}
 	});
+
+	let faultSummary = new Map();
+	$: {
+		$faultsQueryStore?.data?.faults.forEach((fault) => {
+			const current = faultSummary.get(fault.FaultDescription) || 0;
+			faultSummary.set(fault.FaultDescription, current + 1);
+		});
+		console.log([...faultSummary]);
+	}
 </script>
 
 {#if $faultsQueryStore.fetching}
@@ -31,6 +44,10 @@
 {:else if $faultsQueryStore.error}
 	<p>Oopsie! {$faultsQueryStore.error.message}</p>
 {:else}
+	<h1>{$faultsQueryStore.data.faults.length}</h1>
+	{#each [...faultSummary].sort((a, b) => b[1] - a[1]) as [type, count]}
+		<p>{type}: {count}</p>
+	{/each}
 	{#each $faultsQueryStore.data.faults as fault}
 		<p>{fault.ID}</p>
 		<pre>{JSON.stringify(fault)}</pre>

7180796aa03c054ea82780ab2d9f8ae1b20c9664
George <george.dettmer@gmail.com>
2023-11-01 23:01:13 +0000

    Start kitting

---
+++ src/lib/components/Kitting/KitQuantity.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { getContextClient, gql } from '@urql/svelte';
+	import { Button, Input, Label, Radio, Spinner } from 'flowbite-svelte';
+	import { messagesStore } from 'svelte-legos';
+	const urqlClient = getContextClient();
+
+	export let orderItemId: string;
+	export let quantity: number = 0;
+	export let part_id: string | undefined = undefined;
+
+	$: jobInfoStore = subscriptionStore({
+		client: getContextClient(),
+		query: gql`
+			subscription jobKits($jobId: bigint!) {
+				material_jobs_kits(where: { job_id: { _eq: $jobId } }) {
+					id
+					kit_id
+				}
+			}
+		`,
+		variables: { jobId }
+	});
+	$: jobKits = $jobInfoStore?.data?.material_jobs_kits || [];
+
+	let quantityAdding = false;
+	async function addQuantity() {
+		if (quantityAdding) return;
+		if (!$page?.data?.user) {
+			messagesStore('You must be logged in to perform this action', 'warning');
+			return;
+		}
+		if (!quantity) {
+			messagesStore('Quantity to add must be set & be more than 0', 'warning');
+		}
+		/* if (!$page?.data?.user?.processes['eng']) {
+			alert(
+				`You do not have permission to insert components. You have permission for: ${Object.keys(
+					$page?.data?.user?.processes
+				)}`
+			);
+			return;
+		} */
+		quantityAdding = true;
+		let mutationResult;
+
+		mutationResult = await urqlClient.mutation(
+			gql`
+				mutation insertRecievedQty($orderItemId: uuid!, $quantity: Int!, $part_id: text) {
+					insert_erp_kits_items_one(object: { orders_items_id: $orderItemId, quantity: $quantity, part_id: $part_id }) {
+						id
+					}
+				}
+			`,
+			{ orderItemId, quantity, part_id }
+		);
+		if (mutationResult?.error) {
+			console.error('MUTATION ERROR: ', mutationResult);
+			messagesStore('DATABASE ERROR: ' + mutationResult?.error, 'error');
+		} else {
+			console.log('MUTATION RESULT: ', mutationResult);
+			messagesStore('Inserted receipt: ' + mutationResult.data.insert_erp_kits_items_one.id, 'success');
+		}
+		quantityAdding = false;
+	}
+</script>
+
+<div class="flex">
+	<div>
+		<Label for="small-input" class="block mb-2">Quantity</Label>
+		<Input id="small-input" size="sm" placeholder="Kit quantity" bind:value={quantity} />
+		<ul
+			class="items-center w-full rounded-lg border border-gray-200 sm:flex dark:bg-gray-800 dark:border-gray-600 divide-x divide-gray-200 dark:divide-gray-600"
+		>
+			{#each jobKits as kit}
+				<li class="w-full"><Radio name="hor-list" class="p-3">{kit.kit_id}</Radio></li>
+			{:else}
+				<Button>Create new kit</Button>
+			{/each}
+		</ul>
+	</div>
+	<div class="my-2 p-4">
+		<Button color="green" size="sm" on:click={() => addQuantity()} disabled={quantity < 1}>
+			Add ➕
+			{#if quantityAdding}
+				<Spinner class="ml-3" size="3" color="white" />
+			{/if}
+		</Button>
+	</div>
+</div>
--- src/routes/kitting/[[jobId]]/+page.svelte
+++ src/routes/kitting/[[jobId]]/+page.svelte
@@ -2,11 +2,12 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	import { AccordionItem, Accordion, Button, TableBodyCell, TableHeadCell } from 'flowbite-svelte';
+	import { AccordionItem, Accordion, Button, TableBodyCell, TableHeadCell, Modal } from 'flowbite-svelte';
 	import { page } from '$app/stores';
 	import { getContextClient, gql, subscriptionStore } from '@urql/svelte';
 	import { padString } from '$lib/utils';
 	import OrderOverview from '$lib/components/Orders/OrderOverview.svelte';
+	import KitQuantity from '$lib/components/Kitting/KitQuantity.svelte';
 
 	$: jobId = $page?.data?.jobId;
 
@@ -50,7 +51,6 @@
 							supplier {
 								name
 							}
-							tracking
 						}
 						user {
 							id
@@ -110,7 +110,6 @@
 									}
 								}
 							}
-							tracking
 							id
 							supplier {
 								id
@@ -127,13 +126,49 @@
 	$: orders = jobInfo?.jobs_orders || [];
 
 	let expandedOrders: number[] = [];
+
+	let kittingModal = false;
+	let orderItemSelectedQty;
+	let orderItemSelected;
 </script>
+
+<Modal autoclose bind:open={kittingModal}>
+	<div>
+		<KitQuantity orderItemId={orderItemSelected?.id} quantity={orderItemSelectedQty} part_id={orderItemSelected?.part_id} />
+	</div>
+</Modal>
 
 <Accordion multiple flush>
 	{#each orders as { order }}
 		<AccordionItem open={expandedOrders.includes(order?.id)}>
 			<span slot="header">{order?.supplier?.name} ({padString(String(order?.id), 5)})</span>
-			<OrderOverview orderId={order?.id} showRecieved />
+			<OrderOverview orderId={order?.id} showRecieved>
+				<span slot="body">
+					<TableBodyCell>
+						<span
+							class="cursor-pointer"
+							on:click={() => {
+								kittingModal = true;
+								orderItemSelectedQty = remaining;
+								orderItemSelected = item;
+							}}
+						>
+							<!-- <Badge class="mx-0.5" color={!recievedQty ? 'red' : item?.quantity === recievedQty ? 'green' : 'yellow'}>
+								{recievedQty}
+							</Badge> -->
+							<!-- <Badge
+					class="mx-0.5"
+					color={item?.quantity === 0 ? 'red' : item?.quantity === recievedQty ? 'green' : 'yellow'}
+				>
+					<span class="mr-1">➖</span>{recievedQty}<span class="ml-1">➕</span>
+				</Badge> -->
+							{#if item?.quantity !== recievedQty}
+								<span> ➕ </span>
+							{/if}
+						</span>
+					</TableBodyCell></span
+				>
+			</OrderOverview>
 		</AccordionItem>
 	{/each}
 </Accordion>
